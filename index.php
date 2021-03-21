79a7abec946432b443a36cd12b8436139647c340
futebolmultimilionario <79179613+futebolmultimilionario@users.noreply.github.com>
2021-03-21 20:11:38 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
 
 $dados = $_POST;
-
+$requisicao = $_POST;
+  ob_start();
+	var_dump($requisicao);
+	$input = ob_get_contents();
+	ob_end_clean();
+	file_put_contents("input.log",$input.PHP_EOL,FILE_APPEND);
  function cadastraProduto($produto){
   $codigoProduto = $produto['codigo'];
   $nomeProduto = $produto['nome'];
