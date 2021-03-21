412d1edaaed36372f1cb932f398694f6db9725cc
futebolmultimilionario <79179613+futebolmultimilionario@users.noreply.github.com>
2021-03-21 20:13:19 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php
 
 $dados = $_POST;
-$requisicao = $_POST;
   ob_start();
-	var_dump($requisicao);
+	var_dump($dados);
 	$input = ob_get_contents();
 	ob_end_clean();
 	file_put_contents("input.log",$input.PHP_EOL,FILE_APPEND);
